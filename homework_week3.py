443cf4b4f31b6be37b82256d07acb1e6a794d3d3
wnrdkdl <wnrdkdl@naver.com>
2021-10-02 21:51:02 +0900

    commit
    week2 homework modify
    week3 take notes

--- homework_week3.py
+++ homework_week3.py
@@ -12,8 +12,10 @@ soup = BeautifulSoup(data.text, 'html.parser')
 
 trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
+docs = []
 for tr in trs:
     rank = tr.select_one('td.number').text[0:2].strip()
+    # rank = tr.select_one('td.number').text.split()[0] split 함수를 이용하여 필요한 문자열만 가져오기
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     artist = tr.select_one('td.info > a.artist.ellipsis').text
     print( rank, title, artist ) # 달달한 맛 숙제
@@ -23,8 +25,12 @@ for tr in trs:
         'title': title,
         'star': artist
     }
-    db.musicChart.insert_one(doc) # 쓸씁한 맛 숙제
-    
+    # db.musicChart.insert_one(doc) # 쓸씁한 맛 숙제
+    # 위와 같은 방법으로 하면, 데이터 베이스와 반복문을 돌면서 매번 왔다 갔다 함. 이렇게 하면 정보가 많을수록 실행 속도가 느려짐.
+    # 그러므로 docs라는 배열을 반복문 위에 선언하여 한 번에 insert 할 수 있도록 한다.
+    docs.append(doc)
+    db.musicChart.insert_many(docs)
+
     # 반복문으로 출력
     musics = list(db.musicChart.find({'_id' : False}))
 
